3b382c43690c7d7b33b2eecc66105d41d4c1723d
brunchking <francis545@yahoo.com.tw>
2021-11-07 13:59:35 +0800

    first commit

---
+++ src/main/java/com/taiwan/soul/evolution/EvolutionApplication.java
@@ -0,0 +1,13 @@
+package com.taiwan.soul.evolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvolutionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taiwan/soul/brunch/BrunchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taiwan.soul.brunch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrunchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
